0b008aade918ec13bb70a6c69079cbb65d7de45c
nambidurai <nambidurai@gmail.com>
2020-11-20 19:18:30 -0500

    nov update

---
+++ CSharp/ArrayList/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+*/
+using System;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArrayList = new ArrayList();
+            myArrayList.Add(10);
+            myArrayList.Add("this is a string");
+            myArrayList.Add(12.5);
+            myArrayList.AddRange(new ArrayList(){"John",24, 25.8});
+            myArrayList.Insert(1,"fist insert");
+            myArrayList.Insert(3,"third insert");
+            myArrayList.Remove("third insert");
+            myArrayList.Remove(1);
+            myArrayList.Clear();
+            myArrayList.AddRange(new ArrayList(){10,50});
+            myArrayList.Sort();
+            myArrayList.Reverse();
+            Console.WriteLine("array contains 50 ? " + myArrayList.Contains(50));
+            Console.WriteLine("count {0} and capcity {1}",myArrayList.Count, myArrayList.Capacity);
+            myArrayList.Add(25);
+            Console.WriteLine("count {0} and capcity {1}",myArrayList.Count, myArrayList.Capacity);
+
+            foreach (var item in myArrayList)
+            {
+                Console.WriteLine(item);
+            }
+                        
+        }
+    }
+}
