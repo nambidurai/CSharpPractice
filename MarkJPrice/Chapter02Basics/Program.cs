8af6d546b0f537dd2537ac380faf5a8e7d5ff83e
nambidurai <nambidurai@gmail.com>
2020-12-26 06:38:15 -0500

    latest

---
+++ MarkJPrice/Chapter02Basics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; // a semicolon indicates the end of a statement 
+
+namespace Chapter02Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // the start of block
+            Console.WriteLine("Hello World!");
+            // the end of a block
+            // outputs a carriage return 
+            Console.WriteLine();
+            // outputs the greeting and a carriage return 
+            Console.WriteLine("Hello Ahmed");
+            // outputs a formatted number and date and a carriage return 
+            Console.WriteLine(
+                "Temperature on {0:D} is {1}°C.", DateTime.Today, 23.4);
+            double heightInMeters = 1.88;
+            System.Console.WriteLine($@"The variable {nameof(heightInMeters)} 
+has the value {heightInMeters}");
+        }
+    }
+}
---
+++ MarkJPrice/Chapter04WritingFunctions/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace Chapter04WritingFunctions
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table");
+            for (int i = 1; i <=12; i++)
+            {
+                WriteLine($"{i} x {number} = {i*number}");
+            }
+        }
+        static void RunTimesTable()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number between 0 to 255: ");
+                isNumber = byte.TryParse(ReadLine(), out byte number);
+                if (isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    WriteLine("you did not enter a valid number!");
+                }
+            }while (isNumber);
+        }
+        static void Main(string[] args)
+        {
+            RunTimesTable();            
+        }
+    }
+}
